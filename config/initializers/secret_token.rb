fb9623339ced77d76dee312d15336b8a8a9c2e69
Sheng-Loong Su <sushengloong@gmail.com>
2013-07-13 11:06:25 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StockSeasons::Application.config.secret_key_base = 'dd97c162fc2c0b002b38153c1321cadcd3d5e8f6eba83e2e45d5ca252cc4303b82bde3e9d3bc2da811ea02e2eba6dbf1535a2f26a01144f3355a1cb9011f30c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StockSeasons::Application.config.session_store :cookie_store, key: '_stock_seasons_session'
