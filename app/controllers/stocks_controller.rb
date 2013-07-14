b5627c264f0fe5a94aa0d0b149c7dd99dd6fd122
Sheng-Loong Su <sushengloong@gmail.com>
2013-07-14 17:51:41 +0800

    calculate mean of percentage of changes by month

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,6 +4,15 @@ require 'csv'
 class StocksController < ApplicationController
 
   ADJ_CLOSE_INDEX = 6
+  ADJ_CLOSE_CHG_INDEX = 7
+
+  # TODO move into initializer
+  Array.class_eval do
+    def mean
+      a = compact
+      a.sum * 1.0 / a.size
+    end
+  end
 
   def index
   end
@@ -26,6 +35,10 @@ class StocksController < ApplicationController
 
     @tabular_stock_data = @tabular_stock_data.group_by { |d| d[0].strftime('%B') }
 
+    @tabular_stock_data.each do |month, rows|
+      rows.unshift [rows.map{ |r| r[ADJ_CLOSE_CHG_INDEX] }.mean.round(2)]
+    end
+
   rescue OpenURI::HTTPError
     flash[:error] = "cannot retrieve data for stock #{@symbol}"
     render :index
@@ -35,9 +48,9 @@ class StocksController < ApplicationController
 
   def calc_adj_close_changes_pct current_row, prev_row
     if current_row.blank? || prev_row.blank?
-      '-'
+      nil
     else
-      sprintf("%.2f%%", (current_row[ADJ_CLOSE_INDEX] - prev_row[ADJ_CLOSE_INDEX]) / prev_row[ADJ_CLOSE_INDEX] * 100)
+      ((current_row[ADJ_CLOSE_INDEX] - prev_row[ADJ_CLOSE_INDEX]) / prev_row[ADJ_CLOSE_INDEX] * 100).round(2)
     end
   end
 end
